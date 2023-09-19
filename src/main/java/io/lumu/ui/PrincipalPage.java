4c7bb32887b936787968d90a200ed3e0fa134f21
cmoreraz <61637059+cmoreraz@users.noreply.github.com>
2023-09-19 10:36:35 -0500

    Update PrincipalPage.java

--- src/main/java/io/lumu/ui/PrincipalPage.java
+++ src/main/java/io/lumu/ui/PrincipalPage.java
@@ -4,8 +4,10 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class PrincipalPage {
 
-    public static final Target INPUT_TEXT_AREA = Target.the("Input text area").locatedBy("#form div span textarea");
-    public static final Target TOP_COUNTER = Target.the("Top counter").locatedBy("#top_counter div h1 span");
-    public static final Target KEYWORD_DENSITY = Target.the("Input text area").locatedBy("//*[@id='kwd-accordion-data']/a[{0}]");
-
+  public static final Target INPUT_TEXT_AREA =
+      Target.the("Input text area").locatedBy("#form div span textarea");
+  public static final Target TOP_COUNTER =
+      Target.the("Top counter").locatedBy("#top_counter div h1 span");
+  public static final Target KEYWORD_DENSITY =
+      Target.the("Keyword density").locatedBy("//*[@id='kwd-accordion-data']/a[{0}]");
 }
