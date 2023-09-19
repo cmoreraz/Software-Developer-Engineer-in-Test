65a2fe575baa9c82fedede0a7e535577d6b21d68
cmoreraz <61637059+cmoreraz@users.noreply.github.com>
2023-09-19 10:36:42 -0500

    Update CucumberTestSuite.java

--- src/test/java/io/lumu/CucumberTestSuite.java
+++ src/test/java/io/lumu/CucumberTestSuite.java
@@ -7,5 +7,4 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
-public class CucumberTestSuite {
-}
+public class CucumberTestSuite {}
