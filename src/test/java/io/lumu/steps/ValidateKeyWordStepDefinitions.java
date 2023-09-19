7e3463b63694314be1dd03e71765078f3b18e19a
cmoreraz <61637059+cmoreraz@users.noreply.github.com>
2023-09-19 10:36:46 -0500

    Update ValidateKeyWordStepDefinitions.java

--- src/test/java/io/lumu/steps/ValidateKeyWordStepDefinitions.java
+++ src/test/java/io/lumu/steps/ValidateKeyWordStepDefinitions.java
@@ -1,36 +1,28 @@
 package io.lumu.steps;
 
+import static io.lumu.tasks.LeftCount.count;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.lumu.tasks.EnterTextField;
 import io.lumu.tasks.KeywordDensity;
-import io.lumu.tasks.LeftCount;
 import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.Performable;
-
-import static io.lumu.tasks.LeftCount.count;
 
 public class ValidateKeyWordStepDefinitions {
 
-    @When("{actor} fills the text field {string}")
-    public static void fillsTextField(Actor actor, String value){
-        actor.attemptsTo(
-                EnterTextField.enterField(value)
-        );
-    }
+  @When("{actor} fills the text field {string}")
+  public static void fillsTextField(Actor actor, String text) {
+    actor.attemptsTo(EnterTextField.enterField(text));
+  }
 
-    @And("{actor} validate number of words and characters")
-    public static void numberWordsAndCharacters(Actor actor){
-        actor.attemptsTo(
-                count()
-        );
-    }
+  @And("{actor} validate number of words and characters")
+  public static void numberWordsAndCharacters(Actor actor) {
+    actor.attemptsTo(count());
+  }
 
-    @Then("{actor} validate the 3 most repeated words with the number of repetitions")
-    public static void validateMostRepeatedWords(Actor actor){
-        actor.attemptsTo(
-                KeywordDensity.density()
-        );
-    }
+  @Then("{actor} validate the 3 most repeated words with the number of repetitions")
+  public static void validateMostRepeatedWords(Actor actor) {
+    actor.attemptsTo(KeywordDensity.density());
+  }
 }
